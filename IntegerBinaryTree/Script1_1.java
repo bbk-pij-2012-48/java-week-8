ea7fb1f9089c28cb93a315d43654c8cf638f0115
Richard Bennett <rbennett485-gmail-com>
2012-11-16 19:24:08 +0000

    IntegerTreeNode compiles. Adding new nodes works

---
+++ IntegerBinaryTree/Script1_1.java
@@ -0,0 +1,10 @@
+public class Script1_1 {
+	public static void main(String[] args) {
+		IntegerTreeNode first = new IntegerTreeNode(7);
+		first.add(6);
+		first.add(8);
+		first.add(5);
+		first.add(6);
+		first.add(100);
+	}
+}
